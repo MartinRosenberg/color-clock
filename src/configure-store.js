02c21ab7b1e00e5870631bb50e9b912aac4ae9c8
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2018-11-24 11:47:53 -0600

    Remove reference to redux-thunk

--- src/configure-store.js
+++ src/configure-store.js
@@ -1,9 +1,8 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
-import thunk from 'redux-thunk'
+import { combineReducers, compose, createStore } from 'redux'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const configureStore = () => createStore(
   combineReducers({}),
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers()
 )
