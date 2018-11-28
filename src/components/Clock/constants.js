5453b01474d430c2dcf0e4cc4fb026cb1a27f1b1
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2018-11-28 09:10:53 -0600

    Extract style object from Clock component

---
+++ src/components/Clock/constants.js
@@ -0,0 +1,9 @@
+export const style = {
+  alignItems: 'center',
+  color: '#eeeeee',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  height: '100vh',
+  width: '100vw',
+}
--- src/components/Clock/index.jsx
+++ src/components/Clock/index.jsx
@@ -1,22 +1,16 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import { style } from './constants'
+
 export const Clock = ({ color, time }) => (
-  <div style={{
-    alignItems: 'center',
-    color: '#eeeeee',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    height: '100vh',
-    width: '100vw',
-  }}>
+  <div style={style}>
     <h1>{time}</h1>
     <h2>{color}</h2>
   </div>
 )
 
 Clock.propTypes = {
-  time: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
 }
