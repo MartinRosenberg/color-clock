efa87e9b5cd98b2fd60096c837d21ae92f5abaeb
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2018-11-30 16:27:45 -0600

    Manage body with Styled Components; remove Helmet
    Replace React Helmet `body` tag with Styled Components
    `createGlobalStyle`. Remove React Helmet, as it's no longer necessary.

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import Helmet from 'react-helmet'
+import { createGlobalStyle } from 'styled-components'
 
-export const Body = ({ color }) => (
-  <Helmet>
-    <body style={`background: ${color}; transition: background 250ms;`}/>
-  </Helmet>
-)
+export const Body = createGlobalStyle`
+  body {
+    background: ${props => props.color};
+    transition: background-color 250ms;
+  }
+`
 
 Body.propTypes = {
   color: PropTypes.string.isRequired,
